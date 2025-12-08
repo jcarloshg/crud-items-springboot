d65113d254fa1e800c9e0d8782f1c23194e3435b
jcarloshg <carlosj12336@gmail.com>
2025-12-08 12:12:03 -0600

    update unit test

--- src/test/java/com/crud_items/crud_items_back/unit/services/SkillServiceImplTest.java
+++ src/test/java/com/crud_items/crud_items_back/unit/services/SkillServiceImplTest.java
@@ -19,7 +19,9 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class SkillServiceImplTest {
     /**
