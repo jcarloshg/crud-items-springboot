4850af6b247be93ae5cd9f162d4dbf439e06a1a0
jcarloshg <carlosj12336@gmail.com>
2025-12-08 12:16:49 -0600

    update unit test

--- src/test/java/com/crud_items/crud_items_back/unit/services/SkillServiceImplTest.java
+++ src/test/java/com/crud_items/crud_items_back/unit/services/SkillServiceImplTest.java
@@ -17,7 +17,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
